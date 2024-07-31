bec919e2b5c97dd63f2fd6b79939938394f87d36
Jilshad M <dilujilshadvkd@gmail.com>
2024-07-31 22:33:13 +0530

    Update Contact.jsx

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,152 +1,82 @@
-import React, { useRef } from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import styled from "styled-components";
-import emailjs from "@emailjs/browser";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import EarthCanvas from "../canvas/Earth";
+
+
 
 const Container = styled.div`
+  height: 100%;
   display: flex;
-  flex-direction: column;
-  justify-contnet: center;
+  justify-content: center;
   position: rlative;
   z-index: 1;
   align-items: center;
-`;
-
-const Wrapper = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
-  max-width: 1100px;
-  gap: 12px;
   @media (max-width: 960px) {
     flex-direction: column;
   }
-`;
-const Title = styled.div`
-  font-size: 52px;
-  text-align: center;
-  font-weight: 600;
-  margin-top: 20px;
-  color: ${({ theme }) => theme.text_primary};
-  @media (max-width: 768px) {
-    margin-top: 12px;
-    font-size: 32px;
+  @media (max-width: 640px) {
+    flex-direction: column;
   }
-`;
-const Desc = styled.div`
-  font-size: 18px;
-  text-align: center;
-  font-weight: 600;
-  color: ${({ theme }) => theme.text_secondary};
   @media (max-width: 768px) {
-    font-size: 16px;
+    flex-direction: column;
   }
 `;
 
-const ContactForm = styled.div`
-  width: 95%;
-  max-width: 600px;
-  display: flex;
-  flex-direction: column;
-  background-color: rgba(17, 25, 40, 0.83);
-  border: 1px solid rgba(255, 255, 255, 0.125);
-  padding: 32px;
-  border-radius: 12px;
-  box-shadow: rgba(23, 92, 230, 0.1) 0px 4px 24px;
-  margin-top: 28px;
-  gap: 12px;
-`;
-const ContactTitle = styled.div`
-  font-size: 28px;
-  margin-bottom: 6px;
-  font-weight: 600;
-  color: ${({ theme }) => theme.text_primary};
-`;
-const ContactInput = styled.input`
-  flex: 1;
-  background-color: transparent;
-  border: 1px solid ${({ theme }) => theme.text_secondary + 50};
-  outline: none;
-  font-size: 18px;
-  color: ${({ theme }) => theme.text_primary};
-  border-radius: 12px;
-  padding: 12px 16px;
-  &:focus {
-    border: 1px solid ${({ theme }) => theme.primary};
-  }
-`;
-const ContactInputMessage = styled.textarea`
-  flex: 1;
-  background-color: transparent;
-  border: 1px solid ${({ theme }) => theme.text_secondary + 50};
-  outline: none;
-  font-size: 18px;
-  color: ${({ theme }) => theme.text_primary};
-  border-radius: 12px;
-  padding: 12px 16px;
-  &:focus {
-    border: 1px solid ${({ theme }) => theme.primary};
-  }
-`;
-const ContactButton = styled.input`
-  width: 100%;
-  text-decoration: none;
-  text-align: center;
-  background: hsla(271, 100%, 50%, 1);
-  padding: 13px 16px;
-  margin-top: 2px;
-  border-radius: 12px;
-  border: none;
-  color: ${({ theme }) => theme.text_primary};
-  font-size: 18px;
-  font-weight: 600;
-`;
 
 const Contact = () => {
   const form = useRef();
-  const handelSubmit = (e) => {
+
+  const sendEmail = (e) => {
     e.preventDefault();
+
     emailjs
-      .sendForm(
-        "service_1urncpn",
-        "__ejs-test-mail-service__",
-        form.current,
-        "email_WqUgdnPgIPPP2reI9gZwbXWP"
-      )
+      .sendForm('service_1urncpn', 'template_bk7xssd', form.current, {
+        publicKey: 'IRm4y-LQTCzbec5lj',
+      })
       .then(
-        (result) => {
-          alert("Message Sent");
-          form.current.result();
+        () => {
+          console.log('SUCCESS!');
         },
         (error) => {
-          alert(error);
-        }
+          console.log('FAILED...', error.text);
+        },
       );
-  };
-  return (
-    <Container id="Education">
-      <Wrapper>
-        <Title>Contact</Title>
-        <Desc
-          style={{
-            marginBottom: "40px",
-          }}
-        >
-          Feel free to reach out to me for any questions or opportunities!
-        </Desc>
-        <ContactForm onSubmit={handelSubmit}>
-          <ContactTitle>Email Me 🚀</ContactTitle>
-          <ContactInput placeholder="Your Email" name="from_email" />
-          <ContactInput placeholder="Your Name" name="from_name" />
-          <ContactInput placeholder="Subject" name="subject" />
-          <ContactInputMessage placeholder="Message" name="message" rows={4} />
-          <ContactButton type="submit" value="Send" />
-        </ContactForm>
-      </Wrapper>
-    </Container>
-  );
-};
+    };
 
-export default Contact;
+  return (
+    <div>
+      <h1 style={{
+        marginTop:'2rem',
+        textAlign:'center',
+        color:'white',
+        fontWeight:'600'
+      }}>Contact Us</h1>
+    <Container>
+      <form ref={form} onSubmit={sendEmail} style={{
+          color:'white',
+          backgroundColor:'#19243e6d',
+          padding:'3rem 6rem 3rem 6rem',
+          borderRadius: '1rem',
+          margin:'2rem 3rem 3rem 3rem',
+          display:'flex',
+          flexDirection: 'column',
+          justifyContent:'space-between'
+          }}>
+        <label style={{fontSize:'12px'}}>Name</label>
+        <input type="text" name="user_name" className='form-control' placeholder='Your Name' style={{margin:'0 14rem 1rem 0', border:'none'}}/>
+        <label style={{fontSize:'12px'}}>Email</label>
+        <input type="email" name="user_email" className='form-control' placeholder='Your Email' style={{margin:'0 14rem 1rem 0', border:'none'}}/>
+        <label style={{fontSize:'12px'}}>Message</label>
+        <textarea name="message" className='form-control' placeholder='Your Message' style={{margin:'0 14rem 1rem 0', paddingBottom:'2rem', border:'none'}} />
+        <input type="submit" value="Send" className='form-control btn ' style={{backgroundColor:'#513bcc', color:'white'}} />
+        
+      </form>
+      <EarthCanvas />
+    </Container>
+    </div>
+  )
+}
+
+export default Contact
