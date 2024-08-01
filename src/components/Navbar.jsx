94510fbe93bf846518fbfd1e1a1f7232cf3a0c72
Jilshad M <dilujilshadvkd@gmail.com>
2024-08-01 14:01:01 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -132,7 +132,7 @@ const Navbar = () => {
   return (
     <Nav>
       <NavbarContainer>
-        <NavLogo to="/">MyPortfolio</NavLogo>
+        <NavLogo to="/"><b><i>My<i/>Portfolio<b/></NavLogo>
 
         <MobileIcon onClick={() => setIsOpen(!isOpen)}>
           <MenuRounded style={{ color: "inherit" }} />
